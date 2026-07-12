38ecb8e1a040e78f0fb92a7f893314d9cdb3c54c
Mobyzzle <mobizzlebob@proton.me>
2026-07-12 20:25:01 +0200

    Improve image insertion and error handling

---
+++ colors.md
@@ -0,0 +1,5 @@
+
+Regular Output Color: [#FF007F] 
+Underlined, accentation color: [underline #39FF14]
+
+Alert, raisesd execption Color: [#FA003F]
--- src/third_draft.py
+++ src/third_draft.py
@@ -14,17 +14,24 @@ class ImageDatabase:
 
 
     # boilerplate for initiating the Database connection
-    def __init__(self, database_path:str):
+    def __init__(self, database_path:str|Path,debugging:bool=False):
         self.database_path = Path(database_path)
-        
-        self.connection = sqlite3.connect(self.database_path)
 
+        self.connection = sqlite3.connect(self.database_path)
         self.cursor = self.connection.cursor()
 
+        self.debugging = debugging
+        if self.debugging:
+            self.debug("Successfully connected to Database!")
+
+
+    def debug(self,text:str,) -> None:
+        if self.debugging:
+            print(f"[#FF007F]{text}")
+        
 
 
     def get_favourites(self,amount:int=1000) -> list[tuple]:
-
         self.cursor.execute(
             """
             SELECT
@@ -43,23 +50,38 @@ class ImageDatabase:
 
         return self.cursor.fetchall()
     
-    def add_image(self,path:str,title:str="placeholder",*args,**kwargs):
 
-        values1 = args
+    # adding a local image to the databse 
+    # implement features for Links, and kwargs later to set favourites and score later on
+    def add_image(self,file_path:str|Path,title:str,favourite=0,score=0):
+        
+        file = Path(file_path)
+        if file.is_file():
+            try:
+                self.cursor.execute(
+                    """INSERT INTO images_v2 (
+                            title,
+                            file_path,
+                            favourite,
+                            rating)
+                        
+                        VALUES(
+                        ?,?,?,?)""",(title,file_path,favourite,score)
+                )
+                self.connection.commit()
+                self.debug(f"[green]Successfully Uploaded Picture: {self.cursor.lastrowid}/{title}")
+                return self.cursor.lastrowid
+            except sqlite3.Error as E:
+                self.debug(f"[#813E65]Couldn't add Image due to: [underline yellow]{E}")
+                return None
 
-        print(values1)
-
-        values2 = kwargs
-        print(values2.items())
-
+            
+        
+        else:
+            self.debug("[red]couldn't find the file :c")
+            raise FileNotFoundError(file_path) 
+            
     
-
-
-
-
-        pass
-
-
     def close(self):
         self.connection.close()
 
@@ -69,16 +91,16 @@ class ImageDatabase:
 if __name__ == "__main__":
     
     DB_PATH = Path("database/imagevault.db")
-
-    db = ImageDatabase(DB_PATH)
-
-    print("[#FF44CC]Connected!")
-
-    # Testing if the code actually works (it does btw c:)
+  
+    debug = True
+    db = ImageDatabase(DB_PATH,debug)
     
-    print(db.get_favourites())
 
-    db.add_image("file_path","Name",("url","asdjasd"),width=1000,height=1000)
+  
 
+    #Testing if the code actually works (it does btw c:)
+    
 
+    db.add_image("images/this_file_does_not_exist.jpg","flamingo",score=3)
+  
     db.close()
