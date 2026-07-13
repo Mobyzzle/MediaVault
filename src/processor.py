5bfdccfd3eddf0f3005dcaa6ab3c8be8ce918426
Mobyzzle <mobizzlebob@proton.me>
2026-07-13 23:14:08 +0200

    implemented thumbnail creation

--- src/processor.py
+++ src/processor.py
@@ -1,22 +1,47 @@
 # processess and validates images that are put in into an imageIV object, creates thumbnail, reads data, makes the object ready to push into the databank
 from asset import Asset
 import logging
-from PIL import Image
+from PIL import Image, ImageFile
 from pathlib import Path
-
+import os
 logger = logging.getLogger(__name__)
 
 class Processor:
-    def __init__(self):
-        pass
+    
+
+    
+
+    def _create_thumbnail(source:str|Path,output_dir:str|Path,file_name:str) -> Path:
+        try:
+            with Image.open(source) as im:
+                
+                im.thumbnail((150,150))
+                im.save(f"{Path(output_dir)}/{file_name}.webp")
+        except Exception as _e:
+            logger.debug(_e)
+    def _verify_image(source:str|Path) -> None:
+
+        with Image.open(source) as im:
+            im.verify()
 
 
 
     def _get_from_url():
         logger.debug("Not Implemented yet..")
 
-    def _verify_image(self,source:str|Path):
-        pass
+    def _get_from_file(file_path:str|Path) -> dict:
+        source = Path(file_path)
+
+        try:
+            with Image.open(source) as im:
+                return im
+        except Exception as e:
+            logger.debug(e)
+            
+            
+            
+
+
 
 
 
