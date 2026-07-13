def06bb9dae8c3234617ec79127edcc2bb8113c5
Mobyzzle <mobizzlebob@proton.me>
2026-07-13 20:46:11 +0200

    added to_row and from_row functions for asset class

--- src/asset.py
+++ src/asset.py
@@ -1,13 +1,11 @@
 # the actual image class, represents images in the code
 # inherits from PIL.Image, but with added data like ID, Source, file_path, etc. etc.
 from PIL import Image
-import sqlite3
+from sqlite3 import Row
 from pathlib import Path
-steve_data = {'id': 1, 'title': 'Steve', 'file_path': 'images/Steve.jpg',
-               'source_url': None, 'file_size': None, 'width': None, 'height': None, 'date_added': '2026-07-12 20:17:50', 'last_viewed': None,
-                 'viewcount': 0, 'rating': 10, 'favourite': 1, 'thumbnail': None}
 
-# implement dataclass later on, typing this out genuinly made me wanna throw the keyboard
+
+# implement dataclass later on(maybe in the AssetFactory <.<.), typing this out genuinly made me wanna throw the keyboard
 class Asset:
     def __init__(self,id:int=None,title:str = None,file_path:str|Path=None,
                  source_url:str|Path=None,width:int=None,height:int=None,
@@ -15,7 +13,7 @@ class Asset:
                  last_viewed:str=None,viewcount:int=None,file_size:int=None):
         self.id = id 
         self.title = title
-        self.file_path = file_path
+        self.file_path = Path(file_path)
         self.source_url = source_url
         self.width = width
         self.height = height
@@ -23,12 +21,34 @@ class Asset:
         self.last_viewed = last_viewed
         self.viewcount = viewcount
         self.rating = rating
-        self.favourite = favourite
+        self.favourite = bool(favourite)
         self.thumbnail = thumbnail
         self.file_size = file_size
+
+
+    def to_row(self) -> dict[str,object]:
+
+        return {
+            "id": self.id,
+            "title":self.title,
+            "file_path" : Path(self.file_path),
+            "source_url": self.source_url,
+            "width" : self.width,
+            "height": self.height,
+            "date_added" : self.date_added,
+            "last_viewed" : self.last_viewed,
+            "viewcount" : self.viewcount,
+            "rating" : self.rating,
+            "favourite" : self.favourite,
+            "thumbnail" : self.thumbnail,
+            "file_size" : self.file_size
+            
+        }
+
+        pass
         
     @classmethod
-    def from_row(cls,data:dict|sqlite3.Row):
+    def from_row(cls,data:dict|Row):
         return cls(
             id = data["id"],
             title = data["title"],
@@ -47,5 +67,19 @@ class Asset:
         )
 
 if __name__ == "__main__":
+
+
+    steve_data = {'id': 1, 'title': 'Steve', 'file_path': 'images/Steve.jpg',
+               'source_url': None, 'file_size': None, 'width': None, 'height': None, 'date_added': '2026-07-12 20:17:50', 'last_viewed': None,
+                 'viewcount': 0, 'rating': 10, 'favourite': 1, 'thumbnail': None}
+    alex_data = dict(steve_data.items())
+    alex_data["id"] = 2
+
+
     steve = Asset.from_row(steve_data)
+
+    alex = Asset.from_row(alex_data)
+
+    print(steve.to_row(),alex.to_row())
+    
     
