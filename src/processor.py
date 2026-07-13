bf2c5a0e78c293af82ed55892f505d955cf70056
Mobyzzle <mobizzlebob@proton.me>
2026-07-14 00:26:45 +0200

    started implementing add_image

--- src/processor.py
+++ src/processor.py
@@ -1,43 +1,67 @@
 # processess and validates images that are put in into an imageIV object, creates thumbnail, reads data, makes the object ready to push into the databank
 from asset import Asset
 import logging
-from PIL import Image, ImageFile
+from PIL import Image
 from pathlib import Path
 import os
 logger = logging.getLogger(__name__)
 
 class Processor:
     
+    def __init__(self):
+        self.image = Image.Image
+        self.image_file_size = int
+        logger.info("Initialized Processor...")
 
-    
+    def set_image(self,source:Image.Image) -> None:
+        logger.info("Image loaded into Processor....")
+        self.image = source
+        
+    def clear_processor(self) -> None:
+        logger.info("Processor cache cleared")
+        self.image = None
 
-    def _create_thumbnail(source:str|Path,output_dir:str|Path,file_name:str) -> Path:
+    def _create_thumbnail(self,output_dir:str|Path,file_name:str) -> Path:
         try:
-            with Image.open(source) as im:
+            img = self.image.copy()
                 
-                im.thumbnail((150,150))
-                im.save(f"{Path(output_dir)}/{file_name}.webp")
+            img.thumbnail((150,150))
+            output = f"{Path(output_dir)}/{file_name}.webp"
+            img.save(output)
+            logger.info("Thumbnail created...")
+            return Path(output)
         except Exception as _e:
             logger.debug(_e)
-    def _verify_image(source:str|Path) -> None:
-
-        with Image.open(source) as im:
-            im.verify()
 
 
+    def _verify_image(self) -> None:
+        img = self.image.copy()
+        img.verify()
+        logger.info("Image verified...")
 
-    def _get_from_url():
+    
+        
+
+    def _get_from_url(self,):
         logger.debug("Not Implemented yet..")
 
-    def _get_from_file(file_path:str|Path) -> dict:
+    def _get_from_file(self,file_path:str|Path,set:bool=False) -> Image.Image:
         source = Path(file_path)
 
         try:
+
             with Image.open(source) as im:
+                logger.info("Image opened as ImageFile")
+                
+                if set:
+                    self.set_image(im)
+                    return None
                 return im
         except Exception as e:
             logger.debug(e)
-            
+
+    def _get_file_size(self,source:str|Path) -> int:
+        return os.stat(source).st_size
             
             
 
--- src/vault.py
+++ src/vault.py
@@ -3,6 +3,7 @@
 from database import Database
 import logging
 from pathlib import Path
+from asset import Asset
 from processor import Processor
 from exceptions import AppError,not_yet_implemented
 logger = logging.getLogger(__name__)
@@ -16,10 +17,40 @@ class Vault:
             
         else:
             self.db = database
-        
+        self.processor = Processor()
         logger.debug("Initialized Vault...")
 
+    def _build_asset_from_file(self,_title:str,_file_path:str|Path,_source_url:str|Path=None,_rating:int=1,_favourite:bool=False)-> Asset:
+        image = self.processor._get_from_file(_file_path)
+        self.processor.set_image(image)
+        self.processor._verify_image()
 
-    def add_image(self,title:str=None,file_path:str|Path = None,source_url:str=None):
+        thumbnail = self.processor._create_thumbnail()
+        file_size = self.processor._get_file_size(_file_path)
+
+
+        if _favourite:
+            favourite_placeholer = 1
+        else:
+            favourite_placeholer = 0
+
+
+        return Asset(
+            file_path=_file_path,
+            source_url=_source_url,
+            width = image.width,
+            height = image.height,
+            file_size = file_size,
+            thumbnail= thumbnail,
+            favourite=favourite_placeholer,
+            title = _title,
+            rating = _rating,
+
+        )
+
+
+
+    def add_image(self,title:str,file_path:str|Path=None,source_url:tuple[str,bool]=None,rating:int[range(1,10)]=1,favourite:bool=False) -> Asset:
         
-        not_yet_implemented()
+        insertion_asset = self._build_asset_from_file(title,file_path,source_url,rating,favourite)
+        return insertion_asset
